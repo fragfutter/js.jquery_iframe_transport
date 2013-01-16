b05450b1f737452dd60d874e2d02ed6b85f5fea0
fragfutter <fragfutter@gmail.com>
2013-01-16 20:48:09 +0100

    dependency on jquery

--- js/jquery_iframe_transport/__init__.py
+++ js/jquery_iframe_transport/__init__.py
@@ -1,5 +1,8 @@
 from fanstatic import Library, Resource
+import js.jquery
 
 library = Library('jquery_iframe_transport', 'resources')
 
-jquery_iframe_transport = Resource(library, 'jquery.iframe-transport.js')
+jquery_iframe_transport = Resource(library,
+                                   'jquery.iframe-transport.js',
+                                   depends=[js.jquery.jquery, ])
